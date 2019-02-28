fc61dd4b8c4f964b0c566fa23050f88048bfa19a
mex8063 <mex8063@24046-5636.INFO>
2019-02-28 09:40:34 -0300

    Introducao a variaveis

---
+++ 01-variaveis.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  String nome = 'Erik';
+  nome = 'Figueiredo';
+  nome = 'Alves';
+  print(nome);
+
+  int idade = 33;
+  print(idade);
+
+  nome = '33';
+  print(nome);
+
+  double peso = 75.5;
+  print(peso);
+  
+  peso = 75.0;
+  print(peso);
+
+  bool eTutonaSON = false;
+  print(eTutonaSON);
+}
